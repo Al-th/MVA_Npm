e4992a824a640ff28bb1be563746117dc3683db1
paumiern <paumiern.ensimag@gmail.com>
2015-03-16 14:43:11 +0100

    Debut de redaction

--- test2perform.m
+++ test2perform.m
@@ -2,7 +2,7 @@
 clear; clc;
 init;
 
-subsampling = 6;
+subsampling = 10;
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Influence de dmax
@@ -12,12 +12,19 @@ disp('Test influence of d_max on sICP and GICP...');
 %Test on Bunny
 disp('Process on Bunny.asc');
 %Initialization for Bunny
-nbNeighbors = 15;
+nbNeighbors = 20;
 gtTransform = [-10,10,3,0.1,-0.05,0.03];
 [A,covA,B,covB] = setupData(gtTransform(1),gtTransform(2),gtTransform(3),...
                             gtTransform(4),gtTransform(5),gtTransform(6),...
                             nbNeighbors);
-initTransform = zeros(6,1);
+initTransform = gtTransform;
+initTransform(1:3) = degtorad(initTransform(1:3))
+initTransform(1) = initTransform(1)+degtorad(5);
+initTransform(2) = initTransform(2)-degtorad(15);
+initTransform(3) = initTransform(3)-degtorad(10);
+initTransform(4) = initTransform(4);
+initTransform(5) = initTransform(5);
+initTransform(6) = initTransform(6);
 %Subsample 
 A = A(1:subsampling:end,:);
 covA = covA(1:subsampling:end,:,:);
@@ -25,7 +32,7 @@ B = B(1:subsampling:end,:);
 covB = covB(1:subsampling:end,:,:);
 %%
 %Define value of dMax to test
-dMax_bunny = [0.02:0.03:0.2, 0.3:0.1:1];
+dMax_bunny = [0.05:0.03:0.2, 0.3:0.1:1];
 iterMax = [150 50];
 
 %Define param to store results
@@ -33,40 +40,43 @@ info_sicp = {};
 info_gicp = {};
 
 %%
-for i = 1:size(dMax_bunny,2)
+for i = 2:size(dMax_bunny,2)
     fprintf('### d_max : %d, number %d/%d###\n',dMax_bunny(i),i,size(dMax_bunny,2));
     disp('Compute transformation by StandardICP');
     tic;
-    [A_trans,transformation,evol_T,size_subset] = minimization(A,covA,B,covB,...
+    [A_trans,evol_T,size_subset] = minimization(A,covA,B,covB,...
                                               gtTransform, initTransform,... 
                                               iterMax(1),dMax_bunny(i),false);
     elapsed_time = toc;
-    info_sicp{i}.dMax = dMax_bunny(i);
-    info_sicp{i}.time = elapsed_time;
-    info_sicp{i}.average_error = norm(B-A_trans,2);
-    info_sicp{i}.evolution_transformation = evol_T;
-    info_sicp{i}.size_subset = size_subset;
-    info_sicp{i}.estimate_transformation = transformation;
+    info_sicp.dMax = dMax_bunny(i);
+    info_sicp.time = elapsed_time;
+    info_sicp.average_error = norm(B-A_trans,2);
+    info_sicp.evolution_transformation = evol_T;
+    info_sicp.size_subset = size_subset;
+    %info_sicp{i}.estimate_transformation = transformation;
     fprintf('time : %.2f, err : %.2d ,converge in %.0f iterations.\n',...
         elapsed_time,norm(B-A_trans,2),size(size_subset,2));
     
+    save(['./Test/info_dmax_bunny_sicp' int2str(i) '.mat'],'info_sicp');
+    
+    
     disp('Compute transformation by Generalized ICP');
     tic
-    [A_trans,transformation,evol_T,size_subset] = minimization(A,covA,B,covB,...
+    [A_trans,evol_T,size_subset] = minimization(A,covA,B,covB,...
                                               gtTransform, initTransform,... 
                                               iterMax(2),dMax_bunny(i),true);
     elapsed_time = toc;    
-    info_gicp{i}.dMax = dMax_bunny(i);
-    info_gicp{i}.time = elapsed_time;
-    info_gicp{i}.average_error = norm(B-A_trans,2);
-    info_gicp{i}.evolution_transformation = evol_T;
-    info_gicp{i}.size_subset = size_subset;
-    info_gicp{i}.estimate_transformation = transformation;                        
+    info_gicp.dMax = dMax_bunny(i);
+    info_gicp.time = elapsed_time;
+    info_gicp.average_error = norm(B-A_trans,2);
+    info_gicp.evolution_transformation = evol_T;
+    info_gicp.size_subset = size_subset;
+    %info_gicp{i}.estimate_transformation = transformation;                        
     fprintf('time : %.2f, err : %.2d ,converge in %f iterations. \n',...
-        elapsed_time, info_gicp{i}.average_error ,size(size_subset,2));    
+        elapsed_time, info_gicp.average_error ,size(size_subset,2));    
+    save(['./Test/info_dmax_bunny_gicp' int2str(i) '.mat'],'info_gicp');
+    
 end
-save('./Test/info_dmax_bunny_sicp.mat','info_sicp');
-save('./Test/info_dmax_bunny_gicp.mat','info_gicp');
 
 
 disp('SICP and GICP on Bunny done...');
--- test_alex.m
+++ test_alex.m
@@ -1,6 +1,6 @@
 init;
 
-nbNeighbors = 15;
+nbNeighbors = 20;
 gtTransform = [-10,10,3,0.1,-0.05,0.03]
 %A is a bunny
 %B is the A bunny that was subject to a rotation and a translation
@@ -17,7 +17,7 @@ covB = covB(1:10:end,:,:);
 clc
 init;
 
-nbNeighbors = 20
+nbNeighbors = 20;
 [A,covA,poseA] = loadDBData('data/hannover1/scan000',nbNeighbors);
 [B,covB,poseB] = loadDBData('data/hannover1/scan004',nbNeighbors);
 
@@ -49,9 +49,9 @@ initTransform(1:3) = degtorad(initTransform(1:3))
 initTransform(1) = initTransform(1)+degtorad(5);
 initTransform(2) = initTransform(2)-degtorad(15);
 initTransform(3) = initTransform(3)-degtorad(10);
-initTransform(4) = -initTransform(4)+20;
-initTransform(5) = -initTransform(5)+50;
-initTransform(6) = initTransform(6)-30;
+initTransform(4) = initTransform(4);
+initTransform(5) = initTransform(5);
+initTransform(6) = initTransform(6);
 
 
 
