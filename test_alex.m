eca22ca4ae0357c1daf02386bae4387bb2baf7a8
Alexandre This <alexandre.this@gmail.com>
2015-03-17 17:10:00 +0100

    added proof that generalized ICP converges for big ones

--- test_alex.m
+++ test_alex.m
@@ -60,3 +60,32 @@ clf;
 [A_trans,transformation_evolution,size_subset] = minimization(A,covA,B,covB,gtTransform,initTransform,20,0.1,true);
 %[A_trans evol_transform,size_subset]= ICP_ClosedForm(A,B,initTransform,150,0.1);
 computeAverageErrorWithNN(A_trans,B)
+
+%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%Test d'average error sur nuages de points similaires
+clc
+init;
+subSampleStep = 10
+nbNeighbors = 20;
+[A,covA,poseA] = loadDBData('data/hannover1/scan000',nbNeighbors);
+
+A = A(1:subSampleStep:end,:);
+covA = covA(1:subSampleStep:end,:,:);
+
+gtTransform = zeros(1,6);
+
+initTransform = gtTransform;
+
+initTransform(1:3) = degtorad(initTransform(1:3))
+initTransform(1) = initTransform(1)+degtorad(5);
+initTransform(2) = initTransform(2)-degtorad(15);
+initTransform(3) = initTransform(3)-degtorad(10);
+initTransform(4) = initTransform(4)+20;
+initTransform(5) = initTransform(5)-15;
+initTransform(6) = initTransform(6)+10;
+
+close all;
+clf;
+[A_trans,transformation_evolution,size_subset] = minimization(A,covA,A,covA,gtTransform,initTransform,50,1000,true);
+
